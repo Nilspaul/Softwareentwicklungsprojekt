3c0ccb43397c79cd17b0f43fb175cf52748b9db5
Nilspaul <nilspaul27@gmail.com>
2022-10-22 17:12:12 +0200

    console.logs deleted --> userName in Menu

--- imports/ui/components/mainpage/mainpageBase.vue
+++ imports/ui/components/mainpage/mainpageBase.vue
@@ -1,16 +1,28 @@
 <template>
 <v-card class="full-width full-height">
-    <mainContent></mainContent>
+    <div v-if="user">
+        <mainContent :user="user"></mainContent>
+    </div>
 </v-card>
 </template>
 
 <script>
 import mainContent from './subComps/mainContent'
+import {Meteor} from 'meteor/meteor'
+import {Tracker} from 'meteor/tracker'
 export default {
     name: "mainpageBase",
     components: {
     mainContent,
 },
+created() {
+        Tracker.autorun(()=>{
+        this.user = Meteor.user();
+        if(this.user !== undefined){
+            this.$forceUpdate();
+        }
+        })
+    },
 }
 </script>
 
--- imports/ui/components/mainpage/subComps/mainContent.vue
+++ imports/ui/components/mainpage/subComps/mainContent.vue
@@ -1,99 +1,102 @@
 <template>
-<div>
+  <div>
     <v-sheet>
-        <topBar v-on:drawNav="drawNav()"></topBar>
-        <tabs></tabs>
+      <topBar :user="user" v-on:drawNav="drawNav()"></topBar>
+      <tabs></tabs>
+      <v-container class="fill-height">
+        <v-row align="center" justify="center">
+          <v-carousel
+            :class="[{ mobileSlider: $vuetify.breakpoint.mobile }]"
+            cycle
+            show-arrows-on-hover
+          >
+            <v-carousel-item
+              v-for="(item, i) in items"
+              :key="i"
+              :src="item.src"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-row>
+      </v-container>
 
-        <v-container class="fill-height">
-            <v-row align="center" justify="center">
-                <v-carousel  :class="[{'mobileSlider' : $vuetify.breakpoint.mobile}]" cycle show-arrows-on-hover>
-                    <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
-                </v-carousel>
-            </v-row>
-        </v-container>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          </v-list-item-avatar>
 
-        <v-navigation-drawer v-model="drawer" absolute temporary>
-            <v-list-item>
-                <v-list-item-avatar>
-                    <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-                </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-                <v-list-item-content>
-                    <v-list-item-title>John Leider</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
+        <v-divider></v-divider>
 
-            <v-divider></v-divider>
-
-            <v-list dense>
-                <v-list-item v-for="item in items2" :key="item.title" link>
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+        <v-list dense>
+          <v-list-item v-for="item in items2" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </v-sheet>
-</div>
+  </div>
 </template>
 
 <script>
-import topBar from '../../smallComps/topBar'
-import tabs from '../subComps/tabs'
+import topBar from "../../smallComps/topBar";
+import tabs from "../subComps/tabs";
 export default {
-    name: "mainContent",
-    components: {
-        topBar,
-        tabs
+  name: "mainContent",
+  components: {
+    topBar,
+    tabs,
+  },
+  props: {
+    user: Object,
+  },
+  data: () => ({
+    drawer: null,
+    items2: [
+      {
+        title: "Home",
+        icon: "mdi-view-dashboard",
+      },
+      {
+        title: "About",
+        icon: "mdi-forum",
+      },
+    ],
+    items: [
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+      },
+    ],
+  }),
+  methods: {
+    drawNav() {
+      this.drawer = !this.drawer;
     },
-    data: () => ({
-        drawer: null,
-        items2: [{
-                title: 'Home',
-                icon: 'mdi-view-dashboard'
-            },
-            {
-                title: 'About',
-                icon: 'mdi-forum'
-            },
-        ],
-        items: [{
-                src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            },
-            {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-            },
-            {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-            },
-            {
-                src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-            },
-        ],
-
-    }),
-    methods: {
-        drawNav() {
-            this.drawer = !this.drawer;
-        },
-        isMobile(){
-          if(this.$vuetify.breakpoint.md === true){
-            console.log("true")
-            return true
-          }
-        }
+    isMobile() {
+      if (this.$vuetify.breakpoint.md === true) {
+        return true;
+      }
     },
-  
-    mounted () {
-      console.log(this.$vuetify.breakpoint)
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-
-</style>
+<style></style>
