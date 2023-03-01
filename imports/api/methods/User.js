2522739cfb78408bd17aad1548507dcdfe960cd0
Nilspaul <nilspaul27@gmail.com>
2023-03-01 19:38:35 +0100

    deleted nm from user

--- imports/api/methods/User.js
+++ imports/api/methods/User.js
@@ -2,7 +2,7 @@
 import {Meteor} from "meteor/meteor";
 import usersManagement from "../management/UsersManagement"
 import { check } from 'meteor/check'
-import nodemailer from 'nodemailer';
+
 Meteor.methods({
    'user.create'(user) {
       usersManagement.create(user)
