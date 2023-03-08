1f4ea98dc0812595110ab4d3d19e2c75727e54aa
Nilspaul <nilspaul27@gmail.com>
2023-03-08 19:45:53 +0100

    more moduleManagement

--- imports/ui/Views/moduleManagement.vue
+++ imports/ui/Views/moduleManagement.vue
@@ -1,39 +1,96 @@
 <template>
-  <div class="d-flex flex-wrap ma-10">
-    <v-card
-      v-for="(module, index) in modules"
-      height="15em"
-      width="30em"
-      class="mr-4 mb-4"
-    >
-      <v-img
-        @click="changeOverlayVal(index)"
-        :src="module.imageUrl"
-        height="100%"
-        width="100%"
-        :lazy-src="module.imageUrl"
-      >
-      <v-icon color="primary" class="ma-3" style="transform: scale(2);" v-if="checkIfSubscribed(module)">mdi-check-circle-outline</v-icon>
-        <div v-if="!overlay[index]" class="white--text text-h5 modulebar">
-          <div>{{ module.name }}</div>
-        </div>
-      </v-img>
-      <v-overlay
-        absolute
-        :value="overlay[index]"
-        @click="changeOverlayVal(index)"
-      >
-        <v-btn
-        color="secondary" 
-        class="text-h6"
-          v-if="checkIfSubscribed(module)"
-          @click="manageSubscription(module)"
+  <div>
+    <div class="d-flex justify-center">
+      <v-sheet class="ma-2 pa-2">
+        <v-card-title
+          align-center
+          :style="{ color: '#4a5c66' }"
+          class="text-h3 font-weight-bold"
         >
-          UNSUBSCRIBE
-        </v-btn>
-        <v-btn v-else color="secondary" class="text-h6" @click="manageSubscription(module)"> SUBSCRIBE</v-btn>
-      </v-overlay>
-    </v-card>
+          Module Management
+        </v-card-title>
+      </v-sheet>
+    </div>
+    
+      <div class="d-flex flex-wrap justify-center">
+        <v-sheet width="30em">
+          <div class="font-weight-bold secondary--text">Department</div>
+          <v-autocomplete
+            outlined
+            v-model="department"
+            :items="departments"
+            placeholder="Select..."
+            required
+          ></v-autocomplete>
+        </v-sheet>
+        <v-sheet class="ml-4" width="30em">
+          <div class="font-weight-bold secondary--text">Study programm</div>
+          <v-autocomplete
+            outlined
+            v-model="studyProgram"
+            :items="studyPrograms"
+            placeholder="Select..."
+            required
+          ></v-autocomplete>
+        </v-sheet>
+      </div>
+      <div class="d-flex flex-wrap justify-center">
+          <v-sheet width="61em">
+            <div class="font-weight-bold secondary--text">Search module</div>
+            <v-text-field outlined></v-text-field>
+          </v-sheet>
+        </div>
+ 
+
+    <div class="d-flex flex-wrap ma-10">
+      <v-card
+        v-for="(module, index) in modules"
+        height="15em"
+        width="30em"
+        class="mr-4 mb-4"
+      >
+        <v-img
+          @click="changeOverlayVal(index)"
+          :src="module.imageUrl"
+          height="100%"
+          width="100%"
+          :lazy-src="module.imageUrl"
+        >
+          <v-icon
+            color="primary"
+            class="ma-3"
+            style="transform: scale(2)"
+            v-if="checkIfSubscribed(module)"
+            >mdi-check-circle-outline</v-icon
+          >
+          <div v-if="!overlay[index]" class="white--text text-h5 modulebar">
+            <div>{{ module.name }}</div>
+          </div>
+        </v-img>
+        <v-overlay
+          absolute
+          :value="overlay[index]"
+          @click="changeOverlayVal(index)"
+        >
+          <v-btn
+            color="secondary"
+            class="text-h6"
+            v-if="checkIfSubscribed(module)"
+            @click="manageSubscription(module)"
+          >
+            UNSUBSCRIBE
+          </v-btn>
+          <v-btn
+            v-else
+            color="secondary"
+            class="text-h6"
+            @click="manageSubscription(module)"
+          >
+            SUBSCRIBE</v-btn
+          >
+        </v-overlay>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -47,6 +104,30 @@ export default {
     subscribedModules: [],
     absolute: true,
     overlay: [],
+    departments: [
+      "FB 01 - BAU - Bauwesen (Gi)",
+      "FB 02 - EI - Elektro- und Informationstechnik (Gi)",
+      "FB 03 - ME - Maschinenbau und Energietechnik (Gi)",
+      "FB 04 - LSE - Life Science Engineering (Gi)",
+      "FB 05 - GES - Gesundheit (Gi)",
+      "FB 06 - MNI - Mathematik, Naturwissenschaften und Informatik (Gi)",
+      "FB 07 - WIRTSCHAFT - THM Business School (Gi)",
+      "FB 11 - IEM - Informationstechnik-Elektrotechnik-Mechatronik (Fb)",
+      "FB 12 - M - Maschinenbau, Mechatronik, Materialtechnologie (Fb)",
+      "FB 13 - MND - Mathematik, Naturwissenschaften und Datenverarbeitung (Fb)",
+      "FB 14 - WI - Wirtschaftsingenieurwesen (Fb)",
+      "FB 21 - MuK - Management und Kommunikation (Gi,Fb)",
+      "ZDH - Wissenschaftliches Zentrum Duales Hochschulstudium, StudiumPlus (Wz)",
+    ],
+    studyPrograms: [
+      "Infos, Workshops, Organisatorisches",
+      "Bachelor",
+      "Getting Started",
+      "Master",
+      "Forschungs- und Arbeitsgruppen",
+    ],
+    studyProgram: null,
+    department: null,
   }),
   methods: {
     manageSubscription(module) {
@@ -93,7 +174,7 @@ export default {
   align-items: center;
   justify-content: center;
 }
-.v-overlay .v-btn:hover{
+.v-overlay .v-btn:hover {
   background-color: #80ba24 !important;
 }
 </style>
